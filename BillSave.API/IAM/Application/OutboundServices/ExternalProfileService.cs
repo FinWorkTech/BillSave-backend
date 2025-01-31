86c9c628beea55b2cf4479630cde2b1680efe617
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-01-31 18:45:08 -0500

    feat(iam): add external profile service.

---
+++ BillSave.API/IAM/Application/OutboundServices/ExternalProfileService.cs
@@ -0,0 +1,26 @@
+using BillSave.API.Profiles.Interfaces.ACL;
+
+namespace BillSave.API.IAM.Application.OutboundServices;
+
+/// <summary>
+/// External profile service.
+/// </summary>
+/// <param name="profileContextFacade">
+/// Profile context facade.
+/// </param>
+public class ExternalProfileService (IProfilesContextFacade profileContextFacade)
+{
+    /// <summary>
+    /// Creates a new profile.
+    /// </summary>
+    /// <param name="fullName">
+    /// Full name.
+    /// </param>
+    /// <returns>
+    /// The profile identifier.
+    /// </returns>
+    public async Task<int> CreateProfile(string fullName)
+    {
+        return await profileContextFacade.CreateProfileAsync(fullName);
+    }
+}
