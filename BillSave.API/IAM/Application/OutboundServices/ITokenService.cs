d8ba586d4b5dab143a5f9a481a91b4c495e0814e
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-01-31 17:11:51 -0500

    feat(iam): add outbound services.

---
+++ BillSave.API/IAM/Application/OutboundServices/ITokenService.cs
@@ -0,0 +1,31 @@
+using BillSave.API.IAM.Domain.Model.Aggregates;
+
+namespace BillSave.API.IAM.Application.OutboundServices;
+
+/// <summary>
+/// Token service interface 
+/// </summary>
+public interface ITokenService
+{
+    /// <summary>
+    /// Generate token 
+    /// </summary>
+    /// <param name="user">
+    /// The <see cref="User"/> object to generate token for
+    /// </param>
+    /// <returns>
+    /// A string representing the generated token
+    /// </returns>
+    string GenerateToken(User user);
+    
+    /// <summary>
+    /// Validate token 
+    /// </summary>
+    /// <param name="token">
+    /// Token to validate
+    /// </param>
+    /// <returns>
+    /// An integer representing the user id if the token is valid, null otherwise
+    /// </returns>
+    Task<int?> ValidateToken(string token);
+}
