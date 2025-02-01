5d735429a3a72a4be6f6f9953da5e56fcb29501f
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-01 11:38:12 -0500

    feat(sales): add document repository implementation.

---
+++ BillSave.API/Sales/Infrastructure/Persistence/EFC/Repositories/DocumentRepository.cs
@@ -0,0 +1,30 @@
+using Microsoft.EntityFrameworkCore;
+using BillSave.API.Sales.Domain.Repositories;
+using BillSave.API.Sales.Domain.Model.Aggregates;
+using BillSave.API.Shared.Infrastructure.Persistence.EFC.Configuration;
+using BillSave.API.Shared.Infrastructure.Persistence.EFC.Repositories;
+
+namespace BillSave.API.Sales.Infrastructure.Persistence.EFC.Repositories;
+
+/// Document repository.
+/// <summary>
+/// The <see cref="IDocumentRepository"/> repository.
+/// </summary>
+/// <param name="context">
+/// The <see cref="AppDbContext"/> context
+/// </param>
+public class DocumentRepository(AppDbContext context) : BaseRepository<Document>(context),IDocumentRepository
+{
+    /// <inheritdoc />
+    public async Task<IEnumerable<Document>> FindByPortfolioIdAsync(int portfolioId)
+    {
+        return await Context.Set<Document>()
+            .Where(d => d.PortfolioId == portfolioId).ToListAsync();
+    }
+    
+    /// <inheritdoc />
+    public async Task<bool> ExistsById(int id)
+    {
+        return await Context.Set<Document>().AnyAsync(d => d.Id == id);
+    }
+}
