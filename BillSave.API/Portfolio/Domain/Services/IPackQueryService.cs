d1dbe20903cfc90e36ec0d12511f29087c90e73b
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-01 01:33:03 -0500

    feat(portfolio): add commands and queries contracts.

---
+++ BillSave.API/Portfolio/Domain/Services/IPackQueryService.cs
@@ -0,0 +1,21 @@
+using BillSave.API.Portfolio.Domain.Model.Aggregates;
+using BillSave.API.Portfolio.Domain.Model.Queries;
+
+namespace BillSave.API.Portfolio.Domain.Services;
+
+/// <summary>
+/// The service to query packs.
+/// </summary>
+public interface IPackQueryService
+{
+    /// <summary>
+    /// Get a pack by its identifier.
+    /// </summary>
+    /// <param name="query">
+    /// The query to get a pack by its identifier.
+    /// </param>
+    /// <returns>
+    /// The pack.
+    /// </returns>
+    Task<IEnumerable<Pack>> Handle(GetPackByUserIdQuery query);
+}
