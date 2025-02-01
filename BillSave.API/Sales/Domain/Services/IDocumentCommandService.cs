249dfd834d7fbc0a68099f118d632163ad04dad1
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-01 11:24:43 -0500

    feat(sales): add command and query contracts.

---
+++ BillSave.API/Sales/Domain/Services/IDocumentCommandService.cs
@@ -0,0 +1,33 @@
+using BillSave.API.Sales.Domain.Model.Aggregates;
+using BillSave.API.Sales.Domain.Model.Commands;
+
+namespace BillSave.API.Sales.Domain.Services;
+
+/// Document Command Service Interface
+/// <summary>
+/// This interface defines the methods that the Document Command Service must implement.
+/// </summary>
+public interface IDocumentCommandService
+{
+    /// <summary>
+    /// This method is used to get a document by its id.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="CreateDocumentCommand"/> command.
+    /// </param>
+    /// <returns>
+    /// The <see cref="Document"/> object.
+    /// </returns>
+    Task<Document?> Handle(CreateDocumentCommand command);
+    
+    /// <summary>
+    /// This method is used to get a document by its id.
+    /// </summary>
+    /// <param name="command">
+    /// The <see cref="UpdateDocumentCommand"/> command.
+    /// </param>
+    /// <returns>
+    /// The <see cref="Document"/> object.
+    /// </returns>
+    Task<Document?> Handle(UpdateDocumentCommand command);
+}
---
+++ BillSave.API/Sales/Domain/Services/IDocumentQueryService.cs
@@ -0,0 +1,45 @@
+using BillSave.API.Sales.Domain.Model.Aggregates;
+using BillSave.API.Sales.Domain.Model.Commands;
+using BillSave.API.Sales.Domain.Model.Queries;
+
+namespace BillSave.API.Sales.Domain.Services;
+
+/// Document Query Service Interface
+/// <summary>
+/// This interface defines the methods that the Document Query Service must implement.
+/// </summary>
+public interface IDocumentQueryService
+{
+    /// <summary>
+    /// This method is used to get a document by its id.
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetDocumentByDateRangeQuery"/> query.
+    /// </param>
+    /// <returns>
+    /// The <see cref="Document"/> object.
+    /// </returns>
+    Task<Document?> Handle(GetDocumentByDateRangeQuery query);
+    
+    /// <summary>
+    /// This method is used to get a document by its id.
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetDocumentByPortfolioIdQuery"/> query.
+    /// </param>
+    /// <returns>
+    /// The <see cref="Document"/> object.
+    /// </returns>
+    Task<Document?> Handle(GetDocumentByPortfolioIdQuery query);
+    
+    /// <summary>
+    /// This method is used to get a document by its id.
+    /// </summary>
+    /// <param name="query">
+    /// The <see cref="GetDocumentByRateTypeQuery"/> query.
+    /// </param>
+    /// <returns>
+    /// The <see cref="Document"/> object.
+    /// </returns>
+    Task<Document?> Handle(GetDocumentByRateTypeQuery query);
+}
