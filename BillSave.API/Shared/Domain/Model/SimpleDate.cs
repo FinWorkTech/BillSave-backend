35bab890e8174bf1d9bbd099d03568e44cfcdffe
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-01 10:25:02 -0500

    feat(shared): add simple_date value object.

---
+++ BillSave.API/Shared/Domain/Model/SimpleDate.cs
@@ -0,0 +1,17 @@
+namespace BillSave.API.Shared.Domain.Model;
+
+/// Simple date value object
+/// <summary>
+/// This class represents the simple date value object. It is used to store the simple date.
+/// </summary>
+public record SimpleDate()
+{
+    public DateTime Value { get; }
+    
+    public SimpleDate(DateTime value) : this()
+    {
+        Value = value.Date;
+    }
+    
+    public override string ToString() => Value.ToString("yyyy-MM-dd");
+}
