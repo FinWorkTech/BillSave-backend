ac7b0cc367dab738c7cd65d2125610288078fb46
GonzaQuedenaUPC <zquedenaUPC@gmail.com>
2025-02-01 22:41:07 -0500

    feat(portfolio): add portfolio context facade.

---
+++ BillSave.API/Portfolio/Application/ACL/InboundServices/PortfoliosContextFacade.cs
@@ -0,0 +1,64 @@
+using BillSave.API.Portfolio.Interfaces.ACL;
+using BillSave.API.Portfolio.Domain.Repositories;
+
+namespace BillSave.API.Portfolio.Application.ACL.InboundServices;
+
+/// Portfolios context facade.
+/// <summary>
+/// The <see cref="IPortfoliosContextFacade"/> context facade.
+/// </summary>
+/// <param name="packRepository">
+/// The <see cref="IPackRepository"/> repository.
+/// </param>
+public class PortfoliosContextFacade(IPackRepository packRepository) : IPortfoliosContextFacade
+{
+    /// Increment total documents.
+    /// <summary>
+    /// Increment the total documents of a Portfolio.
+    /// </summary>
+    /// <param name="portfolioId">
+    /// The Portfolio id.
+    /// </param>
+    public async Task IncrementTotalDocumentsAsync(int portfolioId)
+    {
+        var pack = await packRepository.FindByIdAsync(portfolioId);
+        
+        if (pack == null)
+            throw new Exception("Portfolio not found");
+
+        try
+        {
+            pack.UpdateTotalDocuments(pack.TotalDocuments + 1);
+            packRepository.Update(pack);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error updating Portfolio", ex);
+        }
+    }
+    
+    /// Decrement total documents.
+    /// <summary>
+    /// Decrement the total documents of a Portfolio.
+    /// </summary>
+    /// <param name="portfolioId">
+    /// The Portfolio id.
+    /// </param>
+    public async Task DecrementTotalDocumentAsync(int portfolioId)
+    {
+        var pack = await packRepository.FindByIdAsync(portfolioId);
+        
+        if (pack == null)
+            throw new Exception("Portfolio not found");
+
+        try
+        {
+            pack.UpdateTotalDocuments(pack.TotalDocuments - 1);
+            packRepository.Update(pack);
+        }
+        catch (Exception ex)
+        {
+            throw new Exception("Error updating Portfolio", ex);
+        }
+    }
+}
---
+++ BillSave.API/Portfolio/Interfaces/ACL/IPortfoliosContextFacade.cs
@@ -0,0 +1,20 @@
+namespace BillSave.API.Portfolio.Interfaces.ACL;
+
+/// Portfolios context facade interface
+/// <summary>
+/// This interface is used to define the methods that will be used to interact with the portfolios context. 
+/// </summary>
+public interface IPortfoliosContextFacade
+{
+    /// Increment total documents
+    /// <summary>
+    /// This method is used to increment the total documents of a portfolio.
+    /// </summary>
+    /// <param name="portfolioId">
+    /// The portfolio id.
+    /// </param>
+    /// <returns>
+    /// A task representing the asynchronous operation.
+    /// </returns>
+    Task IncrementTotalDocumentsAsync(int portfolioId);
+}
